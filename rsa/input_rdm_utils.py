487c5cdd64acbf40f9767180c53f97f9c064a723
Youssef Kashef <youssef.kashef@gmail.com>
2022-01-01 20:42:36 +0100

    1d and 2d

--- rsa/input_rdm_utils.py
+++ rsa/input_rdm_utils.py
@@ -1,9 +1,22 @@
+import os
 import numpy as np
 
+def calc_input_rdm(fpath_src_activations, key=""):
+    """
+    Calculate Input RDM
 
-def calc_input_rdm(fpath_src_activations):
-    acts = np.load(fpath_src_activations)
-    num_samples, _ = acts.shape
+    :param fpath_src_activations: path to .npy file with activations
+    :return: Input RDM
+    """
+    _, ext = os.path.splitext(fpath_src_activations)
+    if ext.endswith('npy'):
+        acts = np.load(fpath_src_activations)
+    elif ext.endswith('h5'):
+        import tables as tb
+        with tb.File(fpath_src_activations, "r") as hf:
+            acts = hf[key]
+    num_samples = acts.shape[0]
+    acts = acts.reshape(num_samples, -1)
     in_rdm = (1 - np.corrcoef(acts, acts, rowvar=True))[:num_samples, num_samples:]
     return in_rdm
 
--- rsa/test_input_rdm_utils.py
+++ rsa/test_input_rdm_utils.py
@@ -1,3 +1,4 @@
+from nose import tools
 from nose.tools import assert_equal, \
     assert_true, assert_false, \
     assert_raises, assert_list_equal
@@ -9,7 +10,8 @@ import numpy as np
 import rsa.input_rdm_utils as input_rdm_utils
 
 
-class TestInputRDMUtils:
+@tools.nottest
+class BaseTestInputRDMUtils:
 
     @classmethod
     def setup_class(cls):
@@ -21,7 +23,26 @@ class TestInputRDMUtils:
         shutil.rmtree(cls.dir_tmp)
         pass
 
+    def test_calc_input_rdm_shape(self):
+        in_rdm = input_rdm_utils.calc_input_rdm(self.fpath_acts)
+        # print(in_rdm)
+        assert_equal(in_rdm.shape[0], self.num_samples)
+        assert_equal(in_rdm.shape[1], self.num_samples)
+
+    def test_calc_input_rdm_values_diag(self):
+        in_rdm = input_rdm_utils.calc_input_rdm(self.fpath_acts)
+        # print(in_rdm)
+        for r_idx, row in enumerate(in_rdm):
+            for c_idx, el in enumerate(row):
+                if r_idx == c_idx:
+                    assert_equal(el, 0.0)
+
+
+@tools.istest
+class TestInputRDMUtils(BaseTestInputRDMUtils):
+
     def setup(self):
+        self.num_samples = 4
         self.activations = np.array([[1, 2, 3],
                                      [4, 5, 6],
                                      [3, 2, 1],
@@ -35,19 +56,40 @@ class TestInputRDMUtils:
             fpath_dst_ret = input_rdm_utils.calc_and_save_input_rdm(self.fpath_acts, fpath_dst_arg)
             assert_equal(fpath_dst_ret, fpath_dst_arg)
 
-    def test_calc_input_rdm_shape(self):
+    def test_calc_input_rdm_values_offdiag(self):
         in_rdm = input_rdm_utils.calc_input_rdm(self.fpath_acts)
-        print(in_rdm)
-        assert_equal(in_rdm.shape[0], 4)
-        assert_equal(in_rdm.shape[1], 4)
+        assert_equal(in_rdm[0, 1], 0)
+        assert_equal(in_rdm[0, 2], 2)
+        assert_equal(in_rdm[0, 3], 2)
+        assert_equal(in_rdm[1, 2], 2)
+        assert_equal(in_rdm[1, 3], 2)
 
 
-    def test_calc_input_rdm_values_diag(self):
-        in_rdm = input_rdm_utils.calc_input_rdm(self.fpath_acts)
-        print(in_rdm)
-        for r_idx, row in enumerate(in_rdm):
-            for c_idx, el in enumerate(row):
-                if r_idx == c_idx:
-                    assert_equal(el, 0.0)
+@tools.istest
+class TestInputRDMUtils2D(BaseTestInputRDMUtils):
 
+    def setup(self):
+        self.num_samples = 3
+        self.activations = np.array([[[1, 2, 3],
+                                      [4, 5, 6]],
+                                     [[11, 12, 13],
+                                      [14, 15, 16]],
+                                     [[16, 15, 14],
+                                      [13, 12, 11]]])
+        self.fpath_acts = os.path.join(self.dir_tmp, 'a2d.npy')
+        np.save(self.fpath_acts, self.activations)
 
+    def test_calc_input_rdm_values_offdiag_2D_input(self):
+        activations = np.array([[[1, 2, 3],
+                                 [4, 5, 6]],
+                                [[11, 12, 13],
+                                 [14, 15, 16]],
+                                [[16, 15, 14],
+                                 [13, 12, 11]]])
+        fpath_acts_2d = os.path.join(self.dir_tmp, 'a2d.npy')
+        np.save(fpath_acts_2d, activations)
+
+        in_rdm = input_rdm_utils.calc_input_rdm(fpath_acts_2d)
+        assert_equal(in_rdm[0, 1], 0)
+        assert_equal(in_rdm[0, 2], 2)
+        assert_equal(in_rdm[1, 2], 2)
