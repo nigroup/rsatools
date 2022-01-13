37a4619ef7e6e0d94669bc3c21d10c463d02e6fd
Youssef Kashef <youssef.kashef@gmail.com>
2022-01-13 16:29:44 +0100

    calculate num of matrix rows from number of upper triangular without diagonal

--- rsa/mat_utils.py
+++ rsa/mat_utils.py
@@ -5,10 +5,15 @@ def get_triu_off_diag_flat(mat):
     return mat[np.triu_indices(mat.shape[0], k=1)]
 
 
-def triu_off_diag_to_mat(triu1vec):
-    nf = 0.5 * (1 + np.sqrt(8 * triu1vec.size + 1))
+def num_mat_rows(num_triu1_elements):
+    nf = 0.5 * (1 + np.sqrt(8 * num_triu1_elements + 1))
     n = int(nf)
     assert (nf == float(n))
+    return n
+
+
+def triu_off_diag_to_mat(triu1vec):
+    n = num_mat_rows(triu1vec.size)
     mat = np.zeros((n, n))
     idx = 0
     for r_idx in range(n):
